0cc5052d2120818839c18ded21fe0766772bf636
ahmedhossam6666 <a.youseff627@gmail.com>
2024-07-19 22:59:34 +0100

    init project.

---
+++ lib/Home/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routName = 'homescreen';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
